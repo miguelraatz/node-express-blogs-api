b404f63f0b9e7352e1ee4883edb04981a406bdec
Miguel Raatz <miguelraatz_@hotmail.com>
2023-05-11 14:14:15 -0300

    feat: implemented model categories from requirement 7

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {*} DataTypes
+ * @returns
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'categories'
+  });
+  return Category;
+};
